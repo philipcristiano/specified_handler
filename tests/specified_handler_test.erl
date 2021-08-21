3a24133571b8fc40c4ac877989fc206eed61127c
Philip Cristiano <git@philipcristiano.com>
2021-08-21 19:18:27 -0400

    Test/fix int error case

--- tests/specified_handler_test.erl
+++ tests/specified_handler_test.erl
@@ -41,7 +41,13 @@ req(Method, Opts) when is_binary(Method) and erlang:is_map(Opts) ->
     MergedReq = maps:merge(Req, Opts),
     MergedReq.
 
-minimal_http_get_test() ->
+read_reply(#{streamid := StreamId}) ->
+    receive
+        {{_Pid, StreamId}, Msg} -> Msg
+    after 10 -> error
+    end.
+
+http_get_test() ->
     load(),
     Metadata = #{
         <<"get">> => #{
@@ -80,9 +86,135 @@ minimal_http_get_test() ->
     meck:expect(?TEST_HANDLER, post_req, ['_', '_'], ok),
 
     Req = req(),
-
     {ok, _Resp, NewState} = ?MUT:upgrade(Req, #{}, ?TEST_HANDLER, #{}),
 
     ?assertEqual(Ref, NewState),
+
+    unload().
+
+http_get_by_param_test() ->
+    load(),
+    Metadata = #{
+        <<"get">> => #{
+            operationId => testOP,
+            tags => [],
+            description => "Gets",
+            parameters => [
+                #{
+                    name => id,
+                    description =>
+                        <<"ID of object">>,
+                    in => query,
+                    schema => #{
+                        type => integer
+                    },
+                    required => true
+                }
+            ],
+            responses => #{
+                200 => #{
+                    description => <<"Test Response">>,
+                    content => #{
+                        'application/json' => #{
+                            schema => #{
+                                type => object,
+                                description => <<"Goal events">>,
+                                properties => #{
+                                    <<"id">> => #{
+                                        type => string,
+                                        description => <<"Collection of feature usage counts">>
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    Trails = [trails:trail("/path", ?TEST_HANDLER, #{}, Metadata)],
+    Ref = make_ref(),
+
+    meck:expect(?TEST_HANDLER, trails, [], Trails),
+    meck:expect(?TEST_HANDLER, handle_req, fun(R, Params, Body, State) ->
+        io:format("Req ~p~n", [{R, Params, Body, State}]),
+        {R, 200, #{}, Ref}
+    end),
+    meck:expect(?TEST_HANDLER, post_req, ['_', '_'], ok),
+
+    Req = req(get, [{<<"id">>, <<"1">>}]),
+
+    io:format("REQ ~p~n", [Req]),
+    {ok, _Resp, NewState} = ?MUT:upgrade(Req, #{}, ?TEST_HANDLER, #{}),
+
+    ?assertEqual(Ref, NewState),
+
+    unload().
+
+http_get_by_param_incorrect_type_int_test() ->
+    load(),
+    Metadata = #{
+        <<"get">> => #{
+            operationId => testOP,
+            tags => [],
+            description => "Gets",
+            parameters => [
+                #{
+                    name => id,
+                    description =>
+                        <<"ID of object">>,
+                    in => query,
+                    schema => #{
+                        type => integer
+                    },
+                    required => true
+                }
+            ],
+            responses => #{
+                200 => #{
+                    description => <<"Test Response">>,
+                    content => #{
+                        'application/json' => #{
+                            schema => #{
+                                type => object,
+                                description => <<"Goal events">>,
+                                properties => #{
+                                    <<"id">> => #{
+                                        type => string,
+                                        description => <<"Collection of feature usage counts">>
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    Trails = [trails:trail("/path", ?TEST_HANDLER, #{}, Metadata)],
+    Ref = make_ref(),
+
+    meck:expect(?TEST_HANDLER, trails, [], Trails),
+    meck:expect(?TEST_HANDLER, handle_req, fun(R, _Params, _Body, _State) ->
+        {R, 200, #{}, Ref}
+    end),
+    meck:expect(?TEST_HANDLER, post_req, ['_', '_'], ok),
+
+    Req = req(get, [{<<"id">>, <<"a">>}]),
+
+    io:format("REQ ~p~n", [Req]),
+    {ok, Resp, _NewState} = ?MUT:upgrade(Req, #{}, ?TEST_HANDLER, #{}),
+    {response, 400, _Headers, Body} = read_reply(Resp),
+
+    RespData = jsx:decode(Body, [return_maps]),
+
+    Expected = #{
+        <<"error">> => #{
+            <<"type_expected">> => <<"integer">>,
+            <<"value">> => <<"a">>,
+            <<"what">> => <<"Incorrect type">>
+        }
+    },
+    ?assertEqual(Expected, RespData),
 
     unload().
